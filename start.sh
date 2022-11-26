39c7671df3ef2bc7f3e72e2cb73609e508b848fe
daviel <david@Air-von-David.lan>
2022-11-26 14:10:08 +0100

    change dir when running start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-SDL_RPI_VIDEO_LAYER=999999 micropython -X heapsize=4M -i ./main.py
+PIGO_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
+cd $PIGO_PATH
+SDL_RPI_VIDEO_LAYER=999999 micropython -X heapsize=4M -i $PIGO_PATH/main.py
