121fb0589a2dddeebeb36d379971b0ebb266089b
David <davidkrawiec@posteo.net>
2026-05-16 16:01:09 +0200

    add sdl app id

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PIGO_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
 cd $PIGO_PATH
-SDL_VIDEODRIVER=wayland micropython -X heapsize=4M $PIGO_PATH/main.py
+SDL_VIDEODRIVER=wayland SDL_APP_ID=pigogui micropython -X heapsize=4M $PIGO_PATH/main.py
