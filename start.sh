e466e1696120faac8a12c7e9dcf525bb55ea6d12
David <davidkrawiec@posteo.net>
2026-05-16 15:45:57 +0200

    remove RPI layer add wayland

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PIGO_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
 cd $PIGO_PATH
-SDL_RPI_VIDEO_LAYER=999999 micropython -X heapsize=4M -i $PIGO_PATH/main.py
+SDL_VIDEODRIVER=wayland micropython -X heapsize=4M $PIGO_PATH/main.py
